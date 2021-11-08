e01d458e50aef116ebe713affaa4d7ea0fd2d6d3
WEBMaximWEB <maxim.geer@yandex.ru>
2021-11-08 21:10:40 +0500

    Выполнение инструкций для очереди
    ExecuteInstractionsToQueue()
    ExecuteInstractionsToStack()

--- Practice3/Program.cs
+++ Practice3/Program.cs
@@ -15,20 +15,23 @@ namespace Practice3
             Queue instractions = Parser.GetFileData(Directory.GetCurrentDirectory() + @"\\..\\..\\.." + @"\input.txt");
             Parser.ExecuteInstractions(instractions);*/
 
-            List list = new List();
+            /*            List list = new List();
 
-            for (int i = 0; i < 6; i++)
-            {
-                list.AddLast(i);
-            }
-            list.AddInIncreasingOrder(3);
-            list.AddInIncreasingOrder(4);
-            list.AddInIncreasingOrder(-1);
-            list.AddInIncreasingOrder(6);
-            list.PrintToConsole();
+                        for (int i = 0; i < 6; i++)
+                        {
+                            list.AddLast(i);
+                        }
+                        list.AddInIncreasingOrder(3);
+                        list.AddInIncreasingOrder(4);
+                        list.AddInIncreasingOrder(-1);
+                        list.AddInIncreasingOrder(6);
+                        list.PrintToConsole();
 
-            list.Replace(-3,3);
-            list.PrintToConsole();
+                        list.Replace(-3,3);
+                        list.PrintToConsole();*/
+
+            Queue inst = Parser.GetFileData("C://test.txt");
+            Parser.ExecuteInstractionsToQueue(inst);
 
             /*list.AddLast('a');
             list.AddLast('b');
